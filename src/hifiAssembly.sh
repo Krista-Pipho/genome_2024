686c191dc76b0707bc755923b71a0c9285ccac40
Angelina Huang <angie.huang@duke.edu>
2024-11-19 10:40:24 -0500

    working on genomescope

--- src/hifiAssembly.sh
+++ src/hifiAssembly.sh
@@ -11,7 +11,7 @@ raw_data=$2
 ../bin/hifiasm -o ../analysis/snake/${output_name} -t48 ${raw_data} 2> ../logs/${output_name}.log
 
 #intput of reads into output .asm assembly file 
-#../bin/hifiasm -o bc2010.asm -t48 /work/alh166/genome_2024/raw_data/m84165_231212_214911_s4.hifi_reads.bc2010.bam.fa 2> bc2010.asm.log
+#../bin/hifiasm -o bc2010.asm -t48 /work/alh166/genome_2024/raw_data/reads/m84165_231212_214911_s4.hifi_reads.bc2010.bam.fa 2> bc2010.asm.log
 #../bin/hifiasm -o bc2009.asm -t48 ../raw_data/m84165_231212_214911_s4.hifi_reads.bc2009.bam.fa 2> bc2009.asm.log
 
 #mv "${snakemake_wildcards[sample]}".bp.p_utg.gfa ../results
--- src/testGenomeScope.sh
+++ src/testGenomeScope.sh
@@ -12,24 +12,18 @@
 
 #/*/genome_2024/*/jellyfish count -C -m $1 -s 1000000000 -t 10 /work/alh166/genome_2024/raw_data/m84165_231212_214911_s4.hifi_reads.bc2010.bam.fa -o /work/alh166/genome_2024/analysis/bc2010.jf 
 
-#desired kmer size
-
-kmer=20
 
 output_name=$1
 raw_data=$2
 
-#../bin/jellyfish count -C -m $kmer -s 1000000000 -t 10 ${raw_data} -o ../analysis/${output_name}.jf
-#../bin/jellyfish histo -t 10 ../analysis/${output_name}.jf > ../analysis/${output_name}.histo
 
 module load R/4.4.0
-conda install r-minpack.lm
+conda install -c r r-minpack.lm
 conda install r-argparse
-#R> install.packages("minpack.lm")
-#conda install minpack.lm
 
-#find docker for genome scope, or figure out why slurm errors on minpack.lm not exisitng
 
 ../bin/genomescope2.0/genomescope.R -i ../analysis/${output_name}.histo -o ../results -k $kmer
 
-#sbatch kmerAnalysis.sh bc2011 ../raw_data/reads/m84165_231212_214911_s4.hifi_reads.bc2011.bam.fa
+#
+#conda install minpack.lm
+#conda install r-minpack.lm
