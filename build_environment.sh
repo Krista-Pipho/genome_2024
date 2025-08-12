99ca247c65fba73d16b94bc31b4eaada9d9c1ead
Krista Pipho <54596000+Krista-Pipho@users.noreply.github.com>
2025-08-12 16:55:55 -0400

    Update build_environment.sh

--- build_environment.sh
+++ build_environment.sh
@@ -3,4 +3,4 @@
 #SBATCH -c32
 #SBATCH --partition=scavenger
 
-conda create -y --name assembly_env --file environment.txt # you can change assembly_env to any name
+conda env create -f environment.txt
